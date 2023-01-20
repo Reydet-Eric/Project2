5d9a49c2bbf339e4b4774728993e2763a4f936af
BEYE ALIOUNE <alioune.beye@gmail.com>
2023-01-20 12:19:56 +0100

    test alioune recette

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,6 +10,7 @@ import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TitleComponent } from './title/title.component';
+import { RecetteComponent } from './recette/recette.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +19,13 @@ import { TitleComponent } from './title/title.component';
     FooterComponent,
     MainComponent,
     NavbarComponent,
-    TitleComponent
+    TitleComponent,
+    RecetteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule,HttpClientModule,
+    Ng2SearchPipeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
